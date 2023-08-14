5915c91fbc09f98fc116340bcd36d74718b7cd19
DESKTOP-A4TEKBE\NERO <2506920977@qq.com>
2023-08-14 15:54:33 +0800

    ast get without text token

--- dist/umd/ember.js
+++ dist/umd/ember.js
@@ -11,18 +11,19 @@
     const startTagOpen = new RegExp(`^<${qnameCapture}`);
     const startTagClose = /^\s*(\/?)>/;
     const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
+    const commentTag = /^<!\--/;
     function parseHtmlToAst(html) {
-      let total = 0;
       let text = html;
       let status = "tag_open";
-      let match = {
-        sel: {},
-        attrs: {},
-        text: ""
-      };
+      let ast = {};
+      let astStack = [];
+      let currentMatch = null;
+      let brotherTag = ["img", "br", "hr", "input", "link", "meta", "param", "source", "track", "comment"];
+      let nextType = "";
       while (text) {
         switch (status) {
           case "tag_open":
+            currentMatch = createAstElement("", {}, "", 0, [], null);
             matchTagOpen();
             break;
           case "tag_start":
@@ -33,46 +34,64 @@
             break;
           case "tag_end":
             matchTagEnd();
-            end(match);
+            break;
+          case "tag_children":
+            matchTagChildren();
+            break;
+          case "tag_brother":
+            matchTagBrother();
+            break;
+          case "tag_father":
+            matchTagFather();
             break;
           case "text":
             matchText();
             break;
           case "tag_close":
-            status = "tag_open";
+            status = "tag_father";
             break;
         }
       }
+      return ast;
       function matchTagOpen() {
-        total++;
         let endTagMatch = text.match(endTag);
-        console.log(total, endTagMatch, text);
         if (!endTagMatch) {
           if (text.indexOf('<') === 0) {
             status = "tag_start";
+          } else {
+            status = "text";
           }
         } else {
-          text = undefined;
-        }
-        if (total > 2) {
-          text = undefined;
+          advance(endTagMatch[0].length);
+          status = "tag_close";
         }
       }
       function matchTagStart() {
         const startTagMatch = text.match(startTagOpen);
+        // normal tag
         if (startTagMatch) {
-          match.sel = startTagMatch[1];
+          currentMatch.sel = startTagMatch[1];
+          currentMatch.type = 1;
           advance(startTagMatch[0].length);
           status = "tag_attribute";
-        } else {
-          advance(text.indexOf('>'));
+        }
+        //abnomal tag such as annotation
+        else {
+          const commentTagMatch = text.match(commentTag);
+          if (commentTagMatch) {
+            currentMatch.sel = "comment";
+            currentMatch.type = 8;
+            currentMatch.text = text.substring(0, text.indexOf('-->'));
+            advance(currentMatch.text.length + 2);
+            currentMatch.text += '-->';
+          }
           status = "tag_end";
         }
       }
       function matchTagAttribute() {
         const attributeMatch = text.match(attribute);
         if (attributeMatch) {
-          match.attrs[attributeMatch[1]] = attributeMatch[3] || attributeMatch[4] || attributeMatch[5];
+          currentMatch.attrs[attributeMatch[1]] = attributeMatch[3] || attributeMatch[4] || attributeMatch[5] || '';
           advance(attributeMatch[0].length);
           status = "tag_attribute";
         } else {
@@ -81,27 +100,80 @@
       }
       function matchTagEnd() {
         const endMatch = text.match(startTagClose);
+        if (brotherTag.includes(currentMatch.sel)) {
+          status = "tag_brother";
+        } else {
+          status = "tag_children";
+        }
         advance(endMatch[0].length);
-        status = "text";
       }
       function matchText() {
-        match.text += text.substring(0, text.indexOf('<'));
-        if (match.text) {
-          advance(match.text.length);
+        let tempText = text.substring(0, text.indexOf('<'));
+        if (tempText) {
+          advance(tempText.length);
+          currentMatch.type = 3;
+          tempText = tempText.trim();
         }
+        status = "tag_brother";
+      }
+      function matchTagChildren() {
+        pushMatch(currentMatch, "children");
         status = "tag_open";
       }
-      function end(match) {
-        console.log("end:", match);
+      function matchTagBrother() {
+        pushMatch(currentMatch, "brother");
+        status = "tag_open";
       }
-
+      function matchTagFather() {
+        pushMatch(currentMatch, "father");
+        status = "tag_open";
+      }
+      function createAstElement(sel, attrs, text, type, children, parent) {
+        return {
+          sel,
+          attrs,
+          text,
+          type,
+          children,
+          parent
+        };
+      }
+      function pushMatch(match, type) {
+        let lastType;
+        if (type === "father") {
+          lastType = nextType;
+          nextType = "father";
+        }
+        if (astStack.length > 0) {
+          if (nextType === "children") {
+            astStack[astStack.length - 1].children.push(match);
+            match.parent = astStack[astStack.length - 1];
+            astStack.push(match);
+          } else if (nextType === "brother") {
+            astStack.pop();
+            astStack[astStack.length - 1].children.push(match);
+            match.parent = astStack[astStack.length - 1];
+            astStack.push(match);
+          } else if (nextType === "father") {
+            if (astStack[astStack.length - 1].children.length !== 0 || lastType !== "children") {
+              astStack.pop();
+            }
+            type = "brother";
+          }
+        } else {
+          ast = match;
+          astStack.push(match);
+        }
+        nextType = type;
+      }
       function advance(length) {
         text = text.substring(length);
       }
     }
 
     function createRenderFunction(html) {
-      parseHtmlToAst(html);
+      const ast = parseHtmlToAst(html);
+      console.log(ast);
     }
 
     // import initState from './initState';
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,6 +1,7 @@
 import { parseHtmlToAst } from "./astParser";
 function createRenderFunction(html) {
     const ast = parseHtmlToAst(html);
+    console.log(ast);
 }
 
 export {
